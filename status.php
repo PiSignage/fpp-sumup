8a37bef6093b2f953f40333bc15ae7bb38854d5b
Michael Jones <michael@pisignage.co.uk>
2026-08-21 18:49:46 +0100

    Fix css src

--- status.php
+++ status.php
@@ -39,7 +39,7 @@ function getTransactionsTotal()
 
 <head>
   <link rel="stylesheet" href="/plugin.php?plugin=fpp-sumup&file=assets/css/mermaid.min.css&nopage=1">
-  <link rel="stylesheet" href="/plugin.php?plugin=fpp-zettle&file=sumup.css&nopage=1">
+  <link rel="stylesheet" href="/plugin.php?plugin=fpp-sumup&file=sumup.css&nopage=1">
   <script type="text/javascript" src="/plugin.php?plugin=fpp-sumup&file=assets/js/gridjs.min.js&nopage=1"></script>
   <script type="text/javascript" src="/plugin.php?plugin=fpp-sumup&file=sumup.js&nopage=1"></script>
   <style>
